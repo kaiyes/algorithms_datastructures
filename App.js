90bcbe8943ad1aeabd8d951c51bb26f1d718f5d5
Kaiyes Ansary <kaiyes.ansary@gmail.com>
2019-07-08 10:41:58 +0600

    empty repo first init

---
+++ App.js
@@ -0,0 +1,67 @@
+import React, { Fragment } from 'react'
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+  TouchableOpacity,
+} from 'react-native'
+
+let fizBuzz = () => {
+  return console.log('hey')
+}
+
+const App = () => {
+  return (
+    <View style={styles.viewContainer}>
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        contentContainerStyle={styles.scrollView}
+      >
+        <TouchableOpacity
+          onPress={fizBuzz}
+          style={styles.button}
+        >
+          <Text style={styles.text}>fizBuzz</Text>
+        </TouchableOpacity>
+        <View style={styles.spacer10} />
+        <TouchableOpacity
+          onPress={fizBuzz}
+          style={styles.button}
+        >
+          <Text style={styles.text}>fizBuzz 2</Text>
+        </TouchableOpacity>
+      </ScrollView>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  viewContainer: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+  scrollView: {
+    flex: 1,
+    backgroundColor: 'white',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  button: {
+    backgroundColor: 'yellow',
+    borderRadius: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    paddingHorizontal: 50,
+    paddingVertical: 20,
+  },
+  spacer10: {
+    height: 10,
+  },
+})
+
+export default App
