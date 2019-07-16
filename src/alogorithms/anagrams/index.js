28c3e9d95771d4f470aa4b18f3bb4e3617c1f807
Kaiyes Ansary <kaiyes.ansary@gmail.com>
2019-07-16 10:54:37 +0600

    anagram 2

--- src/alogorithms/anagrams/index.js
+++ src/alogorithms/anagrams/index.js
@@ -1,36 +1,59 @@
-let anagrams = async () => {
-  //clean & make useable strings
-  // Make 2 character map objects
-  // compare these two
-  const stringA = 'APPLE!'
-  const stringB = 'elppaa'
-  let cleanA = await buildCharMap(stringA)
-  let cleanB = await buildCharMap(stringB)
-  console.log(cleanA, cleanB)
-  if (
-    Object.keys(cleanA).length != Object.keys(cleanB).length
-  ) {
-    console.log('false')
-    return false
-  }
-  for (let char in cleanA) {
-    if (cleanA[char] != cleanB[char]) {
-      console.log('false')
-      return false
-    }
-  }
-  console.log('true')
-  return true
-}
+// let anagrams = async () => {
+//   //clean & make useable strings
+//   // Make 2 character map objects
+//   // compare these two
+//   const stringA = 'APPLE!'
+//   const stringB = 'elppaa'
+//   let cleanA = await buildCharMap(stringA)
+//   let cleanB = await buildCharMap(stringB)
+//   console.log(cleanA, cleanB)
+//   if (
+//     Object.keys(cleanA).length != Object.keys(cleanB).length
+//   ) {
+//     console.log('false')
+//     return false
+//   }
+//   for (let char in cleanA) {
+//     if (cleanA[char] != cleanB[char]) {
+//       console.log('false')
+//       return false
+//     }
+//   }
+//   console.log('true')
+//   return true
+// }
+//
+// let buildCharMap = str => {
+//   let charMap = {}
+//   for (let char of str
+//     .replace(/[^\w]/g, '')
+//     .toLowerCase()) {
+//     !charMap[char] ? (charMap[char] = 1) : charMap[char]++
+//   }
+//   return charMap
+// }
 
-let buildCharMap = str => {
-  let charMap = {}
-  for (let char of str
+const anagram = () => {
+  strA = 'Apple'
+  strB = 'Appl1! '
+
+  cleanA = strA
     .replace(/[^\w]/g, '')
-    .toLowerCase()) {
-    !charMap[char] ? (charMap[char] = 1) : charMap[char]++
-  }
-  return charMap
-}
+    .toLowerCase()
+    .split()
+    .sort()
+    .join('')
+  cleanB = strB
+    .replace(/[^\w]/g, '')
+    .toLowerCase()
+    .split()
+    .sort()
+    .join('')
 
-export default anagrams
+  if (cleanA === cleanB) {
+    console.log('anagram')
+  } else {
+    console.log('not')
+  }
+}
+export default anagram
