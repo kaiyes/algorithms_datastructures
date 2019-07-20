2e0ad930f0c6dbc2cbe9ab60989e75ce3a292956
Kaiyes Ansary <kaiyes.ansary@gmail.com>
2019-07-20 08:39:42 +0600

    tried lcs, slinding window can't solve it

--- alogorithms/longestSubSeq/index.js
+++ alogorithms/longestSubSeq/index.js
@@ -7,50 +7,26 @@ let longestSubStr = async (s1, s2) => {
   // i == j ?  j ++ : i++
   // i   j
   // abad
-  //
   // compare both strings and remove non common chars
-  let arr = cleanArr(s1, s2)
-  let arr1 = arr[0]
-  let arr2 = arr[1]
-  console.log(arr)
+  // let arr = cleanArr(s1, s2)
+
+  let arr1 = s1.split('')
+  let arr2 = s2.split('')
   let i = 0
   let j = 0
   let substr = []
-  while (i < arr1.length) {
-    if ((arr1[i] = arr2[j])) {
-      i++
+  while (i < arr1.length - 1) {
+    if (arr1[i] === arr2[j]) {
       substr.push(arr1[i])
-    } else {
+      i++
       j++
+    } else {
+      j == arr2.length - 1 ? i++ : j++
     }
   }
   console.log(substr.join(''))
 }
-
-let makeObj = str => {
-  bag = {}
-  for (let char of str) {
-    bag[char] ? bag[char]++ : (bag[char] = 1)
-  }
-  return bag
-}
-
-let cleanArr = (str1, str2) => {
-  let s1 = str1.split('')
-  let s2 = str2.split('')
-  for (let char of s1) {
-    if (!s2.includes(char)) {
-      s1.splice(s1.indexOf(char), 1)
-    }
-  }
-  for (let char of s2) {
-    if (!s1.includes(char)) {
-      s2.splice(s2.indexOf(char), 1)
-    }
-  }
-  return [s1, s2]
-}
-
+longestSubStr('ABACCD', 'ACDF')
 longestSubStr('abazdc', 'bacbad')
 
 // let func = async (s1, s2) => {
@@ -66,4 +42,29 @@ longestSubStr('abazdc', 'bacbad')
 //     str1.includes(char) ? null : str2.replace(char, '')
 //   }
 //   console.log(str1, str2)
+// }
+
+//
+// let makeObj = str => {
+//   bag = {}
+//   for (let char of str) {
+//     bag[char] ? bag[char]++ : (bag[char] = 1)
+//   }
+//   return bag
+// }
+//
+// let cleanArr = (str1, str2) => {
+//   let s1 = str1.split('')
+//   let s2 = str2.split('')
+//   for (let char of s1) {
+//     if (!s2.includes(char)) {
+//       s1.splice(s1.indexOf(char), 1)
+//     }
+//   }
+//   for (let char of s2) {
+//     if (!s1.includes(char)) {
+//       s2.splice(s2.indexOf(char), 1)
+//     }
+//   }
+//   return [s1, s2]
 // }
