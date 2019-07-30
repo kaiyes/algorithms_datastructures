8b8f6905ce60a5a458958e32c477792c59e1db6e
Kaiyes Ansary <kaiyes.ansary@gmail.com>
2019-07-30 14:32:47 +0600

    lcs

--- alogorithms/longestSubSeq/index.js
+++ alogorithms/longestSubSeq/index.js
@@ -1,70 +1,33 @@
-let longestSubStr = async (s1, s2) => {
-  // naive solution
-  // =====================
-  // loop over first string
-  // create a window with i and j
-  // "abadc" "bacbad"
-  // i == j ?  j ++ : i++
-  // i   j
-  // abad
-  // compare both strings and remove non common chars
-  // let arr = cleanArr(s1, s2)
+function LCS(S1, m, S2, n) {
+  var finalResult
 
-  let arr1 = s1.split('')
-  let arr2 = s2.split('')
-  let i = 0
-  let j = 0
-  let substr = []
-  while (i < arr1.length - 1) {
-    if (arr1[i] === arr2[j]) {
-      substr.push(arr1[i])
-      i++
-      j++
-    } else {
-      j == arr2.length - 1 ? i++ : j++
-    }
+  //define base case. if the length of either
+  //strings are zero,
+  //then no need to continue return 0
+  if (m === 0 || n === 0) {
+    finalResult = 0
+  } else if (S1[m - 1] === S2[n - 1]) {
+    //if the last characters are the same
+    finalResult = 1 + LCS(S1, m - 1, S2, n - 1)
+  } else {
+    var excludeLastOfS1 = LCS(S1, m - 1, S2, n),
+      excludeLastOfS2 = LCS(S1, m, S2, n - 1)
+
+    finalResult = Math.max(excludeLastOfS1, excludeLastOfS2)
   }
-  console.log(substr.join(''))
+  //return the final result
+  return finalResult
 }
-// longestSubStr('ABACCD', 'ACDF')
-longestSubStr('abazdc', 'bacbad')
 
-// let func = async (s1, s2) => {
-//   let str1 = s1
-//   let str2 = s2
-//   for (let char of str1) {
-//     str2.includes(char)
-//       ? null
-//       : await str1.replace(char, '')
-//   }
-//   console.log(str2)
-//   for (let char of str2) {
-//     str1.includes(char) ? null : str2.replace(char, '')
-//   }
-//   console.log(str1, str2)
-// }
+var string_one = 'ABACBDAB', //BATD
+  string_one_length = string_one.length,
+  string_two = 'BDCADB', //ABACD
+  string_two_length = string_two.length,
+  lcs = LCS(
+    string_one,
+    string_one_length,
+    string_two,
+    string_two_length
+  )
 
-//
-// let makeObj = str => {
-//   bag = {}
-//   for (let char of str) {
-//     bag[char] ? bag[char]++ : (bag[char] = 1)
-//   }
-//   return bag
-// }
-//
-// let cleanArr = (str1, str2) => {
-//   let s1 = str1.split('')
-//   let s2 = str2.split('')
-//   for (let char of s1) {
-//     if (!s2.includes(char)) {
-//       s1.splice(s1.indexOf(char), 1)
-//     }
-//   }
-//   for (let char of s2) {
-//     if (!s1.includes(char)) {
-//       s2.splice(s2.indexOf(char), 1)
-//     }
-//   }
-//   return [s1, s2]
-// }
+console.log('Longest Comment Subsequence is: ', lcs)
