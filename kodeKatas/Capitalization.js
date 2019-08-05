27364b259f784cc7740ab9baac6f7a7c3a1a8b76
Kaiyes Ansary <kaiyes.ansary@gmail.com>
2019-08-05 22:06:04 +0600

    capitalization

---
+++ kodeKatas/Capitalization.js
@@ -0,0 +1,11 @@
+let capitalization = str => {
+  let words = str.split(' ');
+  let newArr = [];
+  for (let word of words) {
+    newArr.push(word.replace(word[0], word[0].toUpperCase()));
+  }
+  return newArr.join(' ');
+};
+
+let res = capitalization('a short function');
+console.log(res);
