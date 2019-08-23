d341e457a0c58df40fedf6dfcf0cae11a439d28c
Kaiyes Ansary <kaiyes.ansary@gmail.com>
2019-08-23 22:07:03 +0600

    fizzBuzz revisited With Swift Comparison

--- alogorithms/fizzBuzz/index.js
+++ alogorithms/fizzBuzz/index.js
@@ -1,29 +1,28 @@
 let fizBuzz = () => {
   for (var i = 1; i <= 100; i++) {
     if (Number.isInteger(i / 15)) {
-      console.log('fizbuzz')
+      console.log('fizbuzz');
     } else if (Number.isInteger(i / 5)) {
-      console.log('fizz')
+      console.log('fizz');
     } else if (Number.isInteger(i / 3)) {
-      console.log('buzz')
+      console.log('buzz');
     } else {
-      console.log(i)
+      console.log(i);
     }
   }
-}
+};
 
 let fizBuzz2 = () => {
-  for (var i = 0; i <= 100; i++) {
+  for (var i = 1; i <= 15; i++) {
     if (i % 3 === 0 && i % 5 === 0) {
-      console.log('fizbuzz')
+      console.log('fizbuzz');
     } else if (i % 3 === 0) {
-      console.log('fizz')
+      console.log('fizz');
     } else if (i % 5 === 0) {
-      console.log('buzz')
-    } else {
-      console.log(i)
+      console.log('buzz');
     }
   }
-}
+};
 
-export default fizBuzz
+console.log(fizBuzz2());
+// export default fizBuzz;
