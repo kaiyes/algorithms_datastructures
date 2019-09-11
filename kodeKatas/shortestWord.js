a439c75e399fe77604116bd638fe415c9dfeff46
Kaiyes Ansary <kaiyes.ansary@gmail.com>
2019-09-11 08:56:49 +0600

    reduced added to shortest word

--- kodeKatas/shortestWord.js
+++ kodeKatas/shortestWord.js
@@ -1,8 +1,13 @@
-// return the shortest word's length in an array
+//Problem  return the shortest word's length in an array.
+// For example, " wide and long sword" ...
+// shortes word length is and.length = 3 so return 3
+s =
+  'turns out random test cases are easier than writing out basic ones'
 
 function findShort(str) {
   // split the string
-  // reduce it
+  // put the words in object map
+  // find the min val from objects map
 
   const words = str.split(' ')
   let bag = {}
@@ -16,7 +21,54 @@ function findShort(str) {
   )
 }
 
-s =
-  'turns out random test cases are easier than writing out basic ones'
+// #####################   Iterative ###############################
 
-console.log(findShort(s))
+function findShort1(s) {
+  return Math.min.apply(
+    null,
+    s.split(' ').map(w => w.length)
+  )
+}
+
+function findShort2(str) {
+  // split the string.
+  // iterate over and save smalles somewhere.
+  // once and for all - differece between this and the first
+  // is that this saves once. But the preivous saves all values
+  // in a hash map.
+  const words = str.split(' ')
+  let smallest = words[0]
+  for (word of words) {
+    if (word.length < smallest.length) {
+      smallest = word
+    }
+  }
+  return smallest.length
+}
+
+// #######################  Reduce #############################
+
+function findShort3(s) {
+  return s
+    .split(' ')
+    .map(a => a.length)
+    .reduce((a, b) => Math.min(a, b))
+}
+
+// #######################  Reduce #############################
+function findReduced(s) {
+  return s
+    .split(' ')
+    .reduce(
+      (acc, val) => (val.length < acc ? val.length : acc),
+      s.split(' ')[0].length
+    )
+}
+
+function findReduced2(s) {
+  return s
+    .split(' ')
+    .reduce((acc, val) => console.log(acc, val.length))
+}
+
+console.log(findReduced(s))
