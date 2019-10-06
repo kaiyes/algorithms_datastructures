5c11e8f5340b3d6eef53dc3fb3a3b074d3804a83
Kaiyes Ansary <kaiyes.ansary@gmail.com>
2019-10-06 09:44:20 +0600

    unmerged commit

--- kodeKatas/divisorBound.js
+++ kodeKatas/divisorBound.js
@@ -1,22 +1,27 @@
-// PROBLEM : write a function that gives the sum of a row
-//           1
-//         3  5
-//       7  9  11
-// rowSum(1) = 1
-// rowSum(2) = 8
-// rowSum(50) = ?
+// Problem : Given the bound (final value) and divisor, find the largest number
+// that is divisable ( by the divisor, dah) and is within the bound. Is greater
+// or equal to the bound.
+// Edge case : None. Guranteed that a divisor is found. Positive Ints only
+// Example : divisorBound(2,9) = 8.
+// 8 is dividable by 2 and is lower than 9
 
-function rowSumOddNumbers(n) {
-  let array = [];
-  for (var i = 1; i < n; i + 2) {
-    array.push(i);
-  }
-  console.log(array);
-  return array;
+function divisorBound(divisor, bound) {
+  const arr = [...Array(bound + 1).keys()];
+  largestNum = arr[0];
+  arr.map(val => {
+    if (val % divisor === 0) {
+      if (val > largestNum) {
+        largestNum = val;
+      }
+    }
+  });
+  return largestNum;
 }
 
-// ########################## Other Methods ##########################
+console.log(divisorBound(2, 8));
+
+// ########################## Other methods ##########################
+
+const maxMultiple = (divisor, bound) => bound - (bound % divisor);
 
 // reduce should work here
-
-console.log(rowSumOddNumbers(4));
